d2ae9f36fcc3edc00936d8af6f7bb8a9ed3bd6fe
SangamSilwal <sangamsilwal2062@gmail.com>
2025-02-25 23:04:44 +0545

    Map in js

--- 20_map.js
+++ 20_map.js
@@ -1,8 +1,8 @@
 const numArray = [1,2,3,4,5,6,7,8,9,10]
-neww = numArray.map((item) => (item*10))
+neww = numArray.map((item) => (item*10))   
 console.log(neww)
 
-
+//Note: Map automatically return a value
 //Chaining of the methods
 new_num = numArray.map((value) => (value*10)).map((item) => (item*10)).filter((value) => value>567)
 console.log(new_num)
