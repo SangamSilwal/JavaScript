aa538c005474bac8324cb27fb71ec3bf99ef040a
SangamSilwal <sangamsilwal2062@gmail.com>
2025-03-15 17:04:49 +0545

    Node js Workflow

---
+++ nodeJs/p3_nodeWorkflow.js
@@ -0,0 +1,11 @@
+/*
+Basically in node their are two type if fun
+Async ---> Non- Blocking 
+synch --> Blocking
+
+
+Blocking: Synchronous operations that halt further execution until the current task completes. Example: fs.readFileSync.
+Non-blocking: Asynchronous operations that allow other tasks to run while waiting. Example: fs.readFile with a callback or promises.
+Non-blocking is key for handling multiple tasks efficiently in Node.js.
+
+*/
