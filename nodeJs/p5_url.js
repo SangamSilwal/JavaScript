8112767e246d7aa21ad4167a35e57e7a3d769d8d
SangamSilwal <sangamsilwal2062@gmail.com>
2025-03-15 21:29:46 +0545

    Learnt about url in nodejs

---
+++ nodeJs/p5_url.js
@@ -0,0 +1,34 @@
+const server = require("http")
+const fs = require("fs")
+const url = require("url")
+
+
+
+const newServer = server.createServer((req,res)=>{
+    if(req.url === "/favicon.ico") return res.end();
+    const text = `Hello --${req.url}--Time: ${Date.now()}\n`
+    const myUrl = url.parse(req.url,true)
+
+// here the myUrl constain the parse value i.e ?search_id=899&name=ibiyank ---> Like this values
+
+    console.log(myUrl)
+    fs.appendFile("test.txt",text,(err,result)=>{
+        switch (myUrl.pathname){
+            case "/":
+                res.end("<h1>Home</h1>");
+                break;
+            case "/about":
+                const name = myUrl.query["nma"]
+                res.end(`<h1>Hi ${name}</h1>`);
+                break;
+            default:
+                res.end("404 Not found");
+                break;
+        }
+    })
+
+})
+
+newServer.listen(8000,()=>{
+    console.log("Listening to the port 8000")
+});
